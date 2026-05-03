4c126a018c0d68ea90cefeff386bfc322d5443cc
Marton Dániel <q6mb8g@tr.pte.hu>
2026-05-03 12:47:06 +0200

    Initialize SAMPLE_BOOKS with book data
    Added a list of sample books with titles, paths, and groups.

---
+++ sample-books.js
@@ -0,0 +1,302 @@
+window.SAMPLE_BOOKS = [
+  {
+    "title": "A Midsummer Knight's Dream",
+    "path": "Books/English Books/A Midsummer Knight's Dream.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Frost",
+    "path": "Books/English Books/Frost.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Gone Astray",
+    "path": "Books/English Books/Gone Astray.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "In the spring",
+    "path": "Books/English Books/In the spring.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Li-Wan the Fair",
+    "path": "Books/English Books/Li-Wan the Fair.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Morning on the Wissahiccon",
+    "path": "Books/English Books/Morning on the Wissahiccon.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Punch, Brothers, Punch!",
+    "path": "Books/English Books/Punch, Brothers, Punch!.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "The Remnants of the Code",
+    "path": "Books/English Books/The Remnants of the Code.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "Transients In Arcadia",
+    "path": "Books/English Books/Transients In Arcadia.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "X-ing a Paragrab",
+    "path": "Books/English Books/X-ing a Paragrab.txt",
+    "group": "English Books"
+  },
+  {
+    "title": "La Confession",
+    "path": "Books/French Books/La Confession.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Le banquet céleste",
+    "path": "Books/French Books/Le banquet céleste.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Le Masque de la mort rouge",
+    "path": "Books/French Books/Le Masque de la mort rouge.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Le Portrait ovale",
+    "path": "Books/French Books/Le Portrait ovale.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Les trois rameaux verts",
+    "path": "Books/French Books/Les trois rameaux verts.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "L'histoire de l'enfant",
+    "path": "Books/French Books/L'histoire de l'enfant.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "L'homme des foules",
+    "path": "Books/French Books/L'homme des foules.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Madame Baptiste",
+    "path": "Books/French Books/Madame Baptiste.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Mademoiselle Fifi",
+    "path": "Books/French Books/Mademoiselle Fifi.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Un duel",
+    "path": "Books/French Books/Un duel.txt",
+    "group": "French Books"
+  },
+  {
+    "title": "Das Manuskript in der Flasche",
+    "path": "Books/German Books/Das Manuskript in der Flasche.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Das Urteil",
+    "path": "Books/German Books/Das Urteil.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Der Heizer",
+    "path": "Books/German Books/Der Heizer.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Der Regenschirm",
+    "path": "Books/German Books/Der Regenschirm.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Ein Landarzt",
+    "path": "Books/German Books/Ein Landarzt.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Mutter Sauvage",
+    "path": "Books/German Books/Mutter Sauvage.txt",
+    "group": "German Books"
+  },
+  {
+    "title": "Bartleby, lo scrivano",
+    "path": "Books/Italian Books/Bartleby, lo scrivano.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "Il cappotto",
+    "path": "Books/Italian Books/Il cappotto.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "Il delitto di lord Arturo Savile",
+    "path": "Books/Italian Books/Il delitto di lord Arturo Savile.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "Il Fantasma Di Canterville",
+    "path": "Books/Italian Books/Il Fantasma Di Canterville.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "L’uomo della sabbia",
+    "path": "Books/Italian Books/L’uomo della sabbia.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "La dama bianca",
+    "path": "Books/Italian Books/La dama bianca.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "Le Avventure Della Notte Di S. Silvestro",
+    "path": "Books/Italian Books/Le Avventure Della Notte Di S. Silvestro.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "L'oratore",
+    "path": "Books/Italian Books/L'oratore.txt",
+    "group": "Italian Books"
+  },
+  {
+    "title": "Аргонавты и золотое руно",
+    "path": "Books/Russian Books/Аргонавты и золотое руно.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Бабья доля",
+    "path": "Books/Russian Books/Бабья доля.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "В киргизских степях",
+    "path": "Books/Russian Books/В киргизских степях.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Дон Кихот и тургеневская девушка",
+    "path": "Books/Russian Books/Дон Кихот и тургеневская девушка.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Дураки",
+    "path": "Books/Russian Books/Дураки.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "За себя или за другую_",
+    "path": "Books/Russian Books/За себя или за другую_.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Как меня обворовывали",
+    "path": "Books/Russian Books/Как меня обворовывали.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Катенька",
+    "path": "Books/Russian Books/Катенька.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Кривые Углы",
+    "path": "Books/Russian Books/Кривые Углы.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Молодняк",
+    "path": "Books/Russian Books/Молодняк.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Мужчины",
+    "path": "Books/Russian Books/Мужчины.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Наказ комиссии о составлении проекта нового уложения",
+    "path": "Books/Russian Books/Наказ комиссии о составлении проекта нового уложения.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Наставление дяди своему племяннику",
+    "path": "Books/Russian Books/Наставление дяди своему племяннику.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Письмо к другу, жительствующему в Тобольске по долгу звания своего",
+    "path": "Books/Russian Books/Письмо к другу, жительствующему в Тобольске по долгу звания своего.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Поэма о голодном человеке",
+    "path": "Books/Russian Books/Поэма о голодном человеке.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Сон",
+    "path": "Books/Russian Books/Сон.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Счастливая любовь",
+    "path": "Books/Russian Books/Счастливая любовь.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Фокус великого кино",
+    "path": "Books/Russian Books/Фокус великого кино.txt",
+    "group": "Russian Books"
+  },
+  {
+    "title": "Alexandre",
+    "path": "Books/Spanish Books/Alexandre.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "Dos amigos",
+    "path": "Books/Spanish Books/Dos amigos.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "El Hombre de la Multitud",
+    "path": "Books/Spanish Books/El Hombre de la Multitud.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "El Regalo de los Reyes Magos",
+    "path": "Books/Spanish Books/El Regalo de los Reyes Magos.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "La aventura de la noche de San Silvestre",
+    "path": "Books/Spanish Books/La aventura de la noche de San Silvestre.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "La boda del lugarteniente Laré",
+    "path": "Books/Spanish Books/La boda del lugarteniente Laré.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "Moiron",
+    "path": "Books/Spanish Books/Moiron.txt",
+    "group": "Spanish Books"
+  },
+  {
+    "title": "Relatos de un cazador",
+    "path": "Books/Spanish Books/Relatos de un cazador.txt",
+    "group": "Spanish Books"
+  }
+];
